96447014529440a8c4d32c39ac059ad693787f3e
Omar Saied <74738267+3marsaied@users.noreply.github.com>
2024-03-14 05:46:26 +0200

    search

--- CROSS/lib/Pages/HomePage.dart
+++ CROSS/lib/Pages/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:reddit_fox/navbar.dart';
+import 'package:reddit_fox/Pages/Search.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,8 +12,25 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
+        title: Text("Home"),
+        centerTitle: false,
         backgroundColor: Colors.black,
         iconTheme: IconThemeData(color: Colors.white),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Search()),
+              );
+            },
+            icon: Icon(Icons.search),
+          ),
+          IconButton( // Avatar IconButton
+            icon: CircleAvatar(),
+            onPressed: () {}, // No action on press
+          ),
+        ],
       ),
       drawer: Drawer(
         width: drawerWidth,
@@ -21,14 +39,14 @@ class HomePage extends StatelessWidget {
           children: [
             ListTile(
               title: Text(
-                "Recently Visited", 
+                "Recently Visited",
                 style: TextStyle(color: Colors.white),
               ),
             ),
           ],
-        ), 
+        ),
       ),
-      endDrawer: Drawer(        
+      endDrawer: Drawer(
         width: userWidth,
         backgroundColor: Colors.deepPurple,
         child: ListView(
@@ -38,6 +56,7 @@ class HomePage extends StatelessWidget {
         ),
       ),
       bottomNavigationBar: nBar(),
+      endDrawerEnableOpenDragGesture: false, // Added to prevent end drawer from opening on swipe
     );
   }
 }
--- CROSS/lib/Pages/Search.dart
+++ CROSS/lib/Pages/Search.dart
@@ -1,14 +1,50 @@
 import 'package:flutter/material.dart';
 
-class Search extends StatelessWidget{
-  const Search({super.key});
+class Search extends StatefulWidget {
+  const Search({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  _SearchState createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  TextEditingController _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void _clearSearch() {
+    _searchController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black,
+        title: TextField(
+          controller: _searchController,
+          decoration: InputDecoration(
+            hintText: 'Search Reddit',
+            filled: true,
+            fillColor: Color.fromARGB(255, 50, 50, 50),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            prefixIcon: Icon(Icons.search),
+            suffixIcon: IconButton(
+              icon: Icon(Icons.clear),
+              onPressed: _clearSearch,
+            ),
+          ),
+          onChanged: (value) {
+            // Handle text field changes
+          },
+        ),
       ),
     );
   }
