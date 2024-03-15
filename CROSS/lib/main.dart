1102133d670d47368b166dcfa9f0fef30b3e39a3
Ahmed7atem <101184690+Ahmed7atem@users.noreply.github.com>
2024-03-15 22:36:15 +0200

    3a4an ana 3aket el dena fa da fix

--- CROSS/lib/main.dart
+++ CROSS/lib/main.dart
@@ -1,32 +1,21 @@
-// ignore_for_file: unused_import
-
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:get/get.dart';
 import 'package:reddit_fox/Configs.dart';
 import 'package:reddit_fox/Pages/Home/HomePage.dart';
 import 'package:reddit_fox/features/auth/screens/EmailVerification/EmailVerificationPage/View/EmailVerificationPage.dart';
-import 'package:reddit_fox/features/auth/screens/StartingPage/View/StartingPage.dart';
-import 'package:reddit_fox/firebase_options.dart';
 
 import 'Shared/AppColors.dart';
 import 'Shared/Fonts/FontModel.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-      ),
-    );
-}
+  await configs();
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  runApp(const Fox());
+  }
+  class Fox extends StatelessWidget {
+  const Fox({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -59,7 +48,7 @@ class MyApp extends StatelessWidget {
           elevation: 0,
         ),
       ),
-      home: const LandingPage(),
+      home: const HomePage(),
     );
   }
 }
