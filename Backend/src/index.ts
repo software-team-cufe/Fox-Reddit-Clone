aebcca1bb86f604d6eb1a28aa86f9e296f22b5b5
Sharif Elmasry <Sharif.Mohammad02@eng-st.cu.edu.eg>
2024-03-16 19:22:13 +0200

    folder structure
    Folder Structure

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,19 +1,11 @@
-//Test the setup and project environment
-// double quotes is changed to single quotes
-// any extra spaces are removed
-// raises an error if the one of the rules is not followed or prettier format is not followed
-// fixed when the file is saved
+require('dotenv').config();
+import express from 'express';
+import config from 'config';
 
-//console.log("Hello World");
+const app = express();
 
-//console.log('Hello World');
-//instead of using relative path in importing(e.g. import add from ../../ etc..), adjust that in the tsconfig file
-//define @src--> start from src
-// eslint-disable-next-line import/default
-import dotenv from 'dotenv-safe';
-import add from './math/add';
+const port = config.get('port');
 
-dotenv.config();
-
-console.log(add(1, 4)); // 3
-console.log(process.env.MY_NAME);
+app.listen(port, () => {
+  console.log(`App started at http://localhost: ${port}`);
+});
