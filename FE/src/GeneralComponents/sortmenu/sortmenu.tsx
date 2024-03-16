4c8e5f904536e3c515cbca80f91170625e07a92f
anas alaa <anas.ibrahim03@eng-st.cu.edu.eg>
2024-03-16 11:58:18 +0200

    one more file cleaned : sortmenu

--- FE/src/GeneralComponents/sortmenu/sortmenu.tsx
+++ FE/src/GeneralComponents/sortmenu/sortmenu.tsx
@@ -2,12 +2,20 @@ import { Fragment, useState } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { ChevronDown } from 'lucide-react'
 
+//for mapping the sorting button options
+const sortings = [
+  { name: 'Hot'},
+  { name: 'New'},
+  { name: 'Top'},
+]
+
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
 export default function Sortmenu({setselected}) {
 
+  //managing current state selection and passing data to parent eleement to change the sorting
   const [current, switchstates] = useState("New");
   const switchup = (period) => {
     switch (period) {
@@ -28,6 +36,8 @@ export default function Sortmenu({setselected}) {
 
   return (
     <Menu as="div" className="relative inline-block text-left ">
+
+      {/* Sort button header*/}
       <div>
         <Menu.Button className="w-full rounded-full inline-flex justify-center gap-x-1.5 bg-white px-3 py-2 text-sm text-gray-900 hover:bg-gray-200">
           {current}
@@ -44,31 +54,24 @@ export default function Sortmenu({setselected}) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
+          
+          {/* Sort options list mapped*/}
         <Menu.Items className="absolute right-0 mt-2 w-20 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="py-1">
+
+            {/* prompt of options*/}
             <div className='font-semibold text-sm mx-3 my-3 text-gray-700'>Sort by</div>
-            <Menu.Item>
-              {({ active }) => (
-                <div className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm', current == 'Hot' ? 'bg-gray-200' : '')} onClick={() => switchup("Hot")}>
-                  Hot
-                </div>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <div className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700','block px-4 py-2 text-sm', current == 'New' ? 'bg-gray-200' : '')}onClick={() => switchup("New")}>
-                  New
-                </div>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <div className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700','block px-4 py-2 text-sm', current == 'Top' ? 'bg-gray-200' : '')}onClick={() => switchup("Top")}>
-                  Top
-                </div>
-              )}
-            </Menu.Item>
-          </div>
+
+            {/* Sort option mapped*/}
+            {sortings.map((sorting, index) => {
+              return (
+                <Menu.Item key={index}>
+                  {({ active }) => (
+                    <div className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm', current === sorting.name ? 'bg-gray-200' : '')} onClick={() => switchup(sorting.name)}>
+                      {sorting.name}
+                    </div>
+                  )}
+                </Menu.Item>);
+            })}
         </Menu.Items>
       </Transition>
     </Menu>
