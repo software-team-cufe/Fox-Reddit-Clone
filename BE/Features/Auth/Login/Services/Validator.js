2cd73c72f114a4d9d2277180cb8b634e99548a77
nouraldeen khalaf <nour.hassan1321@gmail.com>
2024-03-19 23:18:23 +0200

    Commit made from VS Code Azure Static Web Apps

--- BE/Features/Auth/Login/Services/Validator.js
+++
@@ -1,8 +0,0 @@
-const validator = require('../../../../middlewares/validatorMiddleware');
-const { check } = require('express-validator');
-
-module.exports.loginValidator = [
-    check('email').isEmail().trim().withMessage("Please enter a valid email!"),
-    check('password').isLength({ min: 8, max: 200 }).trim().withMessage("Password must be between 8 and 200 characters"),
-    validator,
-];
