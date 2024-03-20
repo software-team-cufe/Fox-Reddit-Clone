3d1b94218a771fc8dbf9ee0d42689318609b472a
Mahmoud Khaled <78917638+mahmoudzkhaledd@users.noreply.github.com>
2024-03-20 22:45:52 +0200

    merge

--- CROSS/lib/core/constants/firebase_constants.dart
+++
@@ -1,6 +0,0 @@
-class FirebaseConstants {
-  static const usersCollection = 'users';
-  static const communitiesCollection = 'communities';
-  static const postsCollection = 'posts';
-  static const commentsCollection = 'comments';
-}
