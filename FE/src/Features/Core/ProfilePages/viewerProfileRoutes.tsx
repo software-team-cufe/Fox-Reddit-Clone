7a809a7b08d7d30588d2cb431f448d76c7bd5259
anas alaa <anas.ibrahim03@eng-st.cu.edu.eg>
2024-03-21 00:49:05 +0200

    fixed sizing with nav and side bar for profile/viewer/search

--- FE/src/Features/Core/ProfilePages/viewerProfileRoutes.tsx
+++ FE/src/Features/Core/ProfilePages/viewerProfileRoutes.tsx
@@ -41,9 +41,9 @@ function Layout() {
   return (
     <div>
       {/* main header with avatar and username */}
-      <div className="flex ml-56 flex-row gap-10">
-        <div role="avatarHeader" className="flex-1 min-w-fill">
-        <div className='relative flex mb-8'>
+      <div className="flex gap-10">
+        <div className="w-fill flex-1 ">
+        <div role="avatarHeader" className='relative flex mb-8'>
         <img src={'/mySnoo.png'} className='p-1 w-20 h-24 rounded-full z-0' alt=""></img>
         <span className='text-black font-bold text-2xl absolute top-10 left-24'>user</span>
         <span className='text-gray-500 font-semibold absolute top-3/4 left-24'>u/user</span>
