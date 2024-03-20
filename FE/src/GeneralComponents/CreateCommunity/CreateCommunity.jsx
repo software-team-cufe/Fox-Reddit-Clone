345a84145ce1d892d97b956e5787afa4ddb887e8
anas alaa <anas.ibrahim03@eng-st.cu.edu.eg>
2024-03-21 00:24:57 +0200

    fixed create

--- FE/src/GeneralComponents/CreateCommunity/CreateCommunity.jsx
+++ FE/src/GeneralComponents/CreateCommunity/CreateCommunity.jsx
@@ -69,10 +69,10 @@ export default function CreateCommunity({ onClose = () => { } }) {
     return (
         <>
             {(
-                <div className="relative" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+                <div className="relative z-50" aria-labelledby="modal-title" role="dialog" aria-modal="true">
                     <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
                     <div className="fixed inset-0 w-screen overflow-y-auto">
-                        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0 ">
                             <div role="createForm" className="relative transform overflow-hidden rounded-xl w-80 h-[635px] bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
 
