242a316ce57c0591f29f2dd7d8be33217f591541
nouraldeen khalaf <nour.hassan1321@gmail.com>
2024-03-21 00:41:33 +0200

    DEV-ops was here connecting the server

--- BE/Features/AppRoutes.js
+++
@@ -1,9 +0,0 @@
-const appRoute = require('express').Router();
-const authRoutes = require('./Auth/AuthRoutes');
-const { userValidatorMiddleware } = require('../middlewares/UserValidatorMiddleware');
-
-
-appRoute.use("/", userValidatorMiddleware, authRoutes);
-
-
-module.exports = appRoute;
