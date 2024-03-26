5773a25972773b0cfb1842ba521b198221e7fead
NouranAloui <120125352+NouranAloui@users.noreply.github.com>
2024-03-26 16:48:21 +0200

    merge main

--- FE/src/GeneralComponents/SettingApp/settingapp.jsx
+++ FE/src/GeneralComponents/SettingApp/settingapp.jsx
@@ -1,10 +1,10 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import Navofsetting from './navofsetting';
 import EmailSetting from './EmailSetting';
 import { Route, Routes } from "react-router-dom";
 
 export default [
-  <Route key={"/setting"} path="/setting" element={<Navofsetting />} /> ,
-  <Route key={"/setting/email"} path="/setting/email" element={<EmailSetting />} />,
+  <Route key={"/setting"} path="/setting" element={<Navofsetting />} />,
+  <Route key={"/setting/email"} path="/setting/email" element={<EmailSetting />} />
 ]
-        
+
