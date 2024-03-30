ef49cd354e7ed89fcc23c0bce5ddec9d21d1e2e5
Hossam Yasser <hossamyasseramer@gmail.com>
2024-03-30 17:06:30 +0200

    unit testing

--- CROSS/lib/features/auth/screens/login_screen.dart
+++ CROSS/lib/features/auth/screens/login_screen.dart
@@ -6,11 +6,36 @@ import 'package:reddit_fox/core/common/CustomButton.dart';
 import 'package:reddit_fox/core/common/CustomTextBox.dart';
 import 'package:reddit_fox/features/auth/screens/ForgetPasswordScreen.dart';
 
-class LoginScreen extends StatelessWidget {
-  final TextEditingController emailOrPhoneController = TextEditingController();
+class LoginScreen extends StatefulWidget {
+  static login(final String email, final String password) {
+    String messages = '';
+    if (email.trim().isEmpty) messages = "$messages Please enter email";
+    RegExp emailpaatern = RegExp(
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+    if (!emailpaatern.hasMatch(email)) {
+      messages = "$messages,Please enter a valid email";
+    }
+
+    if (password.trim().isEmpty) messages = "$messages,Please enter password";
+    if (password.length < 7) {
+      messages = "$messages,Please enter more than 7 characters password";
+    }
+    if (messages == '') return null;
+    return messages;
+  }
+
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailController = TextEditingController();
+
   final TextEditingController passwordController = TextEditingController();
 
-  LoginScreen({super.key});
+  String? errormessage;
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +63,7 @@ class LoginScreen extends StatelessWidget {
               CustomTextBox(
                 hintText: "Email or Phone number",
                 icon: Icons.email,
-                controller: emailOrPhoneController, // Added controller
+                controller: emailController, // Added controller
               ),
               const Gap(10),
               CustomTextBox(
@@ -47,18 +72,18 @@ class LoginScreen extends StatelessWidget {
                 controller: passwordController, // Added controller
               ),
               const Gap(20),
+              if (errormessage != null) Text(errormessage!),
               CustomButton(
-                text: "Login",
-                onTap: () {
-                  // Access entered text using controllers:
-                  String emailOrPhone = emailOrPhoneController.text;
-                  String password = passwordController.text;
-                  Get.to(() => const HomePage()); // temporarily
-                  
-
-                  // Perform login logic with the retrieved text
-                },
-              ),
+                  text: "Login",
+                  onTap: () {
+                    setState(() {
+                      errormessage = LoginScreen.login(
+                          emailController.text, passwordController.text);
+                    });
+                    if (errormessage == null) {
+                      Get.to(() => const HomePage());
+                    }
+                  }),
               const Gap(20),
               TextButton(
                 onPressed: () {
--- CROSS/test/LoginValidator.dart
+++ CROSS/test/LoginValidator.dart
@@ -5,14 +5,14 @@ class LoginValidator {
         r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
     if (!emailpaatern.hasMatch(value)) return "Please enter a valid email";
 
-    return 'email is valid';
+    return null;
   }
 
   static passwordValidator(final String value) {
     if (value.trim().isEmpty) return "Please enter password";
-
     if (value.length < 7) return "Please enter more than 7 characters";
+    return null;
 
-    return 'password is valid';
+
   }
 }
