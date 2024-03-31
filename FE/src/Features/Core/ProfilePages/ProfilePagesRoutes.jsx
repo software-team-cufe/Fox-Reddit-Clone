a1b1de59a5a3a1463bc198a7e7a521aea6cab48c
nouraldeen khalaf <nour.hassan1321@gmail.com>
2024-03-31 13:11:56 +0200

    anas zorar el search

--- FE/src/Features/Core/ProfilePages/ProfilePagesRoutes.jsx
+++ FE/src/Features/Core/ProfilePages/ProfilePagesRoutes.jsx
@@ -1,4 +1,11 @@
-import { Link, Outlet, Route, Routes, useLocation, useParams, } from "react-router-dom";
+import {
+  Link,
+  Outlet,
+  Route,
+  Routes,
+  useLocation,
+  useParams,
+} from "react-router-dom";
 import ProfileOverview from "./pages/profileoverview";
 import { Plus } from "lucide-react";
 import ProfileUpvoted from "./pages/profileupvoted";
@@ -12,7 +19,7 @@ import PeriodSelect from "@/GeneralComponents/PeriodSelect/PeriodSelect";
 import Card from "@/GeneralComponents/profileCard/Card.jsx";
 import React, { useState } from "react";
 import { userStore } from "@/hooks/UserRedux/UserStore";
-import {useContext,createContext} from "react";
+import { useContext, createContext } from "react";
 
 // for mapping the list of buttons
 const buttons = [
@@ -44,7 +51,7 @@ const buttons = [
     text: "downvoted",
     path: "downvoted",
   },
-]
+];
 
 export const ProfileContext = createContext({
   selected: "New",
@@ -53,90 +60,158 @@ export const ProfileContext = createContext({
   setperiod: (period) => {},
 });
 
-
 // Create a provider component that holds the state
 export function ProfileProvider({ children }) {
   const [selected, setselected] = useState("New");
   const [period, setperiod] = useState("All time");
 
   return (
-    <ProfileContext.Provider value={{ selected, setselected, period, setperiod }}>
+    <ProfileContext.Provider
+      value={{ selected, setselected, period, setperiod }}
+    >
       {children}
     </ProfileContext.Provider>
   );
 }
 
 function Layout() {
-  
   const path = useLocation();
-  const { selected} = useContext(ProfileContext);
-  const user = userStore.getState().user.user;    // fetching user info from redux store
-  const avatar = userStore.getState().user.avatar;  // fetching user avatar from redux store
+  const { selected } = useContext(ProfileContext);
+  const user = userStore.getState().user.user; // fetching user info from redux store
+  const avatar = userStore.getState().user.avatar; // fetching user avatar from redux store
 
   return (
     <div className="w-full">
       {/* main header with avatar and username */}
       <div className="flex gap-10">
-      <div className="flex-1 w-full">
-      <div role="avatarHeader" className='relative w-full flex mb-8'>
-        <img src={avatar} className='p-1 w-20 h-24 rounded-full z-0' alt=""></img>
-        <span className='text-black font-bold text-2xl absolute top-10 left-24'>u/{user}</span>
-        <span className='text-gray-500 font-semibold absolute top-3/4 left-24'>u/{user}</span>
-      </div>
+        <div className="flex-1 w-full">
+          <div role="avatarHeader" className="relative w-full flex mb-8">
+            <img
+              src={avatar}
+              className="p-1 w-20 h-24 rounded-full z-0"
+              alt=""
+            ></img>
+            <span className="text-black font-bold text-2xl absolute top-10 left-24">
+              u/{user}
+            </span>
+            <span className="text-gray-500 font-semibold absolute top-3/4 left-24">
+              u/{user}
+            </span>
+          </div>
 
-      {/* selection of user activity: posts,comments...etc*/}
-      <ul role ="sectionsBar" className='flex gap-3 overflow-x-auto mb-3 p-1'>
-        {
-          buttons.map((btn, index) => <li key={index}>
-            <Link role={`${btn.text}Button`} to={`/user/${user}/${btn.path}`}>
-              <button  className={`rounded-3xl w-fit px-3 h-10 hover:underline hover:bg-gray-300 ${path.pathname == `/user/${user}/${btn.path}` ? "bg-gray-300" : "bg-white"}`} >{btn.text}</button>
-            </Link>
-          </li>)
-        }
-      </ul>
+          {/* selection of user activity: posts,comments...etc*/}
+          <ul
+            role="sectionsBar"
+            className="flex gap-3 overflow-x-auto mb-3 p-1"
+          >
+            {buttons.map((btn, index) => (
+              <li key={index}>
+                <Link
+                  role={`${btn.text}Button`}
+                  to={`/user/${user}/${btn.path}`}
+                >
+                  <button
+                    className={`rounded-3xl w-fit px-3 h-10 hover:underline hover:bg-gray-300 ${
+                      path.pathname == `/user/${user}/${btn.path}`
+                        ? "bg-gray-300"
+                        : "bg-white"
+                    }`}
+                  >
+                    {btn.text}
+                  </button>
+                </Link>
+              </li>
+            ))}
+            <li>
+              <Link role={`searchButton`} to={`/search/posts`}>
+                <button
+                  className={`rounded-3xl w-fit px-3 h-10 hover:underline hover:bg-gray-300`}
+                >
+                  search
+                </button>
+              </Link>
+            </li>
+          </ul>
 
+          {/* sorting lists and period select components and create post in case of overview*/}
+          <div className="flex gap-1">
+            {/* create post button in case of overview */}
+            <button
+              role="createPostButton"
+              className={`rounded-full flex gap-1 justify-center border border-gray-600 w-fit px-4 h-10 items-center hover:border-black ${
+                path.pathname == "/user/overview" ? "" : "hidden"
+              }`}
+            >
+              <Plus className="w-4 h-4" />
+              <span className="inline font-semibold text-sm">
+                Create a post
+              </span>
+            </button>
 
-      {/* sorting lists and period select components and create post in case of overview*/}
-      <div className="flex gap-1">
-        {/* create post button in case of overview */}
-        <button role="createPostButton" className={`rounded-full flex gap-1 justify-center border border-gray-600 w-fit px-4 h-10 items-center hover:border-black ${path.pathname == '/user/overview' ? "" : "hidden"}`} >
-          <Plus className="w-4 h-4"/>
-          <span className='inline font-semibold text-sm'>Create a post</span>
-        </button>
-
-          {/* sorting lists and period select components */}
-          <div role="sortmenu"><Sortmenu context={ProfileContext}/></div>
-          <PeriodSelect appearance={selected} context={ProfileContext}/>
-
-        </div>
-        <hr/>
+            {/* sorting lists and period select components */}
+            <div role="sortmenu">
+              <Sortmenu context={ProfileContext} />
+            </div>
+            <PeriodSelect appearance={selected} context={ProfileContext} />
+          </div>
+          <hr />
           <Outlet />
-      </div>
+        </div>
 
-      {/* profile user card */}
-      <div role="card"><Card/></div>
+        {/* profile user card */}
+        <div role="card">
+          <Card />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default function ProfilePagesLayout() {
-  const { user } = useParams();  // fetching user from url
+  const { user } = useParams(); // fetching user from url
   return (
     // nested routing for the profile pages renders layout then feed according to route
     <ProfileProvider>
-    <Routes>
-        <Route element={<Layout />} >
-        <Route key={'/user'} path='/' element={<></>} />
-        <Route key={'/hidden'} path="/hidden" element={<ProfileHidden using={user}/>} />
-        <Route key={'/saved'} path="/saved" element={<ProfileSaved using={user}/>} />
-        <Route key={'/comments'} path="/comments" element={<ProfileComments context={ProfileContext} using={user}/>} />
-        <Route key={'/posts'} path="/posts" element={<ProfilePosts context={ProfileContext} using={user}/>} />
-        <Route key={'/overview'} path="/overview" element={<ProfileOverview using={user} />} />
-        <Route key={'/upvoted'} path="/upvoted" element={<ProfileUpvoted using={user}/>} />
-        <Route key={'/downvoted'} path="/downvoted" element={<ProfileDownvoted using={user}/>} />
-      </Route>
-    </Routes>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route key={"/user"} path="/" element={<></>} />
+          <Route
+            key={"/hidden"}
+            path="/hidden"
+            element={<ProfileHidden using={user} />}
+          />
+          <Route
+            key={"/saved"}
+            path="/saved"
+            element={<ProfileSaved using={user} />}
+          />
+          <Route
+            key={"/comments"}
+            path="/comments"
+            element={<ProfileComments context={ProfileContext} using={user} />}
+          />
+          <Route
+            key={"/posts"}
+            path="/posts"
+            element={<ProfilePosts context={ProfileContext} using={user} />}
+          />
+          <Route
+            key={"/overview"}
+            path="/overview"
+            element={<ProfileOverview using={user} />}
+          />
+          <Route
+            key={"/upvoted"}
+            path="/upvoted"
+            element={<ProfileUpvoted using={user} />}
+          />
+          <Route
+            key={"/downvoted"}
+            path="/downvoted"
+            element={<ProfileDownvoted using={user} />}
+          />
+        </Route>
+      </Routes>
     </ProfileProvider>
-  )
+  );
 }
--- FE/src/Features/Core/ProfilePages/pages/profilecomments.jsx
+++ FE/src/Features/Core/ProfilePages/pages/profilecomments.jsx
@@ -1,73 +1,81 @@
 import React, { useEffect, useState, useContext } from "react";
-import axios from 'axios';
+import axios from "axios";
 import CommentComponent from "@/GeneralComponents/Comment/CommentComponent";
 import { comment } from "postcss";
 import Spinner from "@/GeneralElements/Spinner/Spinner";
 
-function ProfileComments({using, context}) {
+function ProfileComments({ using, context }) {
+  // states for collecting comments from request and loading state
+  const { selected, period } = useContext(context);
+  const [comments, setComments] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    // states for collecting comments from request and loading state
-    const { selected, period } = useContext(context);
-    const [comments, setComments] = useState([]);
-    const [loading, setLoading] = useState(true);
+  //fetch comments on load and put into comments array
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("http://localhost:3002/comments")
+      //axios.get('https://virtserver.swaggerhub.com/BOUDIE2003AHMED/fox/1/user/sharif29/comments?page=4&count=10&limit=50&t=month')
+      .then((response) => {
+        const newComments = response.data.map((comment) => ({
+          user: {
+            image: comment.user.avatar,
+            Username: comment.user.username,
+            id: comment.user.userID,
+          },
+          info: {
+            votes: comment.votesCount,
+            time: comment.createdAt,
+          },
+          content: {
+            text: comment.commentText,
+          },
+        }));
 
-    //fetch comments on load and put into comments array
-    useEffect( () => {
-        setLoading(true);
-        axios.get("http://localhost:3002/comments")
-        //axios.get('https://virtserver.swaggerhub.com/BOUDIE2003AHMED/fox/1/user/sharif29/comments?page=4&count=10&limit=50&t=month')
-            .then(response => {
-                const newComments = response.data.map(comment => ({
-                    user: {
-                        image: comment.user.avatar,
-                        Username: comment.user.username,
-                        id: comment.user.userID
-                    },
-                    info: {
-                        votes: comment.votesCount,
-                        time: comment.createdAt,
-                    },
-                    content: {
-                        text: comment.commentText
-                    }
-                }));
+        setComments(newComments);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setLoading(false);
+      });
+  }, [period, selected]);
 
-                setComments(newComments);
-                setLoading(false);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                setLoading(false);
-            });
-    }, [period, selected]);
-
-    //to handle waiting for fetch or loading state
-    if (loading) {
-        return(
-            <div role="commentstab" className="w-100 h-100 flex flex-col items-center justify-center">
-            <Spinner className="h-24 w-24"/>
-            </div>
-        )
-    }
-
-    //main comments feed
+  //to handle waiting for fetch or loading state
+  if (loading) {
     return (
-        <div role="commentstab" className="flex flex-col w-full h-fit my-4 items-center">
+      <div
+        role="commentstab"
+        className="w-100 h-100 flex flex-col items-center justify-center"
+      >
+        <Spinner className="h-24 w-24" />
+      </div>
+    );
+  }
 
-            {/* if there are no comments, show no results */}
-            {comments.length > 0 ? (
-                comments.map((comment, index) => (
-                    <CommentComponent key={index} comment={comment} />
-                ))
-            ) : (
-                <>
-                    {/*no results view*/}
-                    <img src={'/confusedSnoo.png'} className="w-16 h-24 mb-2" alt="Confused Snoo"></img>
-                    <p className="text-lg font-bold">looks like you haven't commented on anything</p>
-                </>
-            )}
-        </div>
-    )
+  //main comments feed
+  return (
+    <div role="commentstab" className="flex flex-col w-full h-fit my-4">
+      {/* if there are no comments, show no results */}
+      {comments.length > 0 ? (
+        comments.map((comment, index) => (
+          <CommentComponent key={index} comment={comment} />
+        ))
+      ) : (
+        <>
+          {/*no results view*/}
+          <img
+            src={"/confusedSnoo.png"}
+            className="w-16 h-24 mb-2"
+            alt="Confused Snoo"
+          ></img>
+          <p className="text-lg font-bold">
+            looks like you haven't commented on anything
+          </p>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default ProfileComments;
