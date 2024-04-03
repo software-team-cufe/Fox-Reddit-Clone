cb8e7aafb2649c9e0390f4a7aa0825b29606d0a4
AnAs101AlAa <anas.ibrahim03@eng-st.cu.edu.eg>
2024-04-03 23:01:38 +0200

    seperated community page footers in separate file (nadine work goes there)

--- FE/src/Features/Core/CommunityPage/CommunityPage.jsx
+++ FE/src/Features/Core/CommunityPage/CommunityPage.jsx
@@ -5,9 +5,10 @@ import { useParams, Link, useLocation } from "react-router-dom";
 import axios from "axios";
 import Spinner from '@/GeneralElements/Spinner/Spinner';
 import PostComponent from "@/GeneralComponents/Post/Post";
-import { Plus, Circle } from 'lucide-react';
+import { Plus } from 'lucide-react';
 import bellMenu from "./accessories/bellmenu";
 import optionsMenu from "./accessories/optionsmenu";
+import MainFooter from "./footers/mainFooter";
 //helping functions for the notifications frequency and options menu
 
 export const CommunityContext = createContext({
@@ -180,30 +181,7 @@ export default function CommunityPage() {
         </div>
 
         {/* community description and rules and other tools on the right*/}
-        <div className='w-[340px] min-w-[25%] flex-auto h-fit p-3 overflow-y-scroll bg-gray-100 rounded-lg hidden md:block'>
-          {/*maint title and about*/}
-          <div className="text-sm font-bold">title</div>
-          <span className="text-xs text-gray-599">{comm.description}</span>
-
-          {/*community statistics ans such*/}
-          <div className="grid grid-cols-2 mt-3 grid-rows-2 grid-flow-rows justify justify-between">
-            <div className="text-sm font-semibold text-gray-500">{comm.membersCount}</div>
-            <div className="text-sm font-semibold text-gray-500">{comm.onlineMembers}</div>
-            <div className="text-sm font-semibold text-gray-800">members</div>
-            <div className="flex gap-1 text-sm font-semibold text-gray-800"><Circle className="w-2 h-2 fill-green-500 text-green-500 mt-[6px]" />online</div>
-          </div>
-          <hr className="w-full border-1 my-2 border-gray-300" />
-
-          {/* mapping of community rules*/}
-          {comm.rules.length > 0 && (<>
-            <div className="text-sm font-bold">Rules</div>
-            <ul className="text-xs text-gray-599">
-              {
-                comm.rules.map((rule, index) => <li key={index} className="list-disc list-inside my-1">{rule}</li>)
-              }
-            </ul>
-            <hr className="w-full border-1 my-2 border-gray-300" /></>)}
-        </div>
+        <MainFooter comm={comm} />
       </div>
     </div>
   )
