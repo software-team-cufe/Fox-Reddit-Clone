754c77c5f86bee958779e93238a8302a28182609
nouraldeen khalaf <nour.hassan1321@gmail.com>
2024-04-08 16:50:55 +0200

    delete front and back

--- FE/src/hooks/UserRedux/UserStore.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './UserModelSlice';
-export const userStore = configureStore({
-    reducer: {
-        user: userReducer,
-    }
-});
-
-
