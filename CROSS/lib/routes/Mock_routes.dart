e4e7ec1f60f1a32eb8d87dd79a05ce322993357e
Hossam Yasser <hossamyasseramer@gmail.com>
2024-04-12 00:12:11 +0200

    user token

--- CROSS/lib/routes/Mock_routes.dart
+++ CROSS/lib/routes/Mock_routes.dart
@@ -1,5 +1,13 @@
+// class ApiRoutes {
+//   static const String Popular = '$baseUrl/trendingSearches';
+// }
+
 class ApiRoutes {
-  static const String baseUrl = 'http://localhost:3000/';
-  static const String Popular = '$baseUrl/trendingSearches';
-  
+  static const String baseUrl = 'https://json-server-k6zb.onrender.com';
+  // static const String baseUrl = 'http://localhost:3000';
+
+  static const String getPopular = baseUrl;
+  static const String getPosts = '$baseUrl/posts';
+  static const String login = '$baseUrl/user';
+  static const String getUserById = '$baseUrl/user/{id}';
 }
