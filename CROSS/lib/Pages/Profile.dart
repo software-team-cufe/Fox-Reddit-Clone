38e6281dbd3353e12e42fd231157ebe15df897fd
Omar Saied <74738267+3marsaied@users.noreply.github.com>
2024-04-12 16:04:18 +0200

    .........

--- CROSS/lib/Pages/Profile.dart
+++ CROSS/lib/Pages/Profile.dart
@@ -2,36 +2,66 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:typed_data';
+import 'package:intl/intl.dart'; 
 import 'package:reddit_fox/Pages/Search.dart';
 import 'package:reddit_fox/routes/Mock_routes.dart';
-import 'package:reddit_fox/Pages/Profile.dart';
+import 'package:reddit_fox/Pages/Profile.dart'; 
 import 'package:share/share.dart';
 
-class Profile extends StatefulWidget {
-  const Profile({Key? key, required this.user_Id}) : super(key: key);
+class ProfilePage extends StatefulWidget {
+  ProfilePage({Key? key, required this.user_Id}) : super(key: key);
   final int user_Id;
 
   @override
-  _ProfileState createState() => _ProfileState();
+  _ProfilePageState createState() => _ProfilePageState();
 }
 
-class _ProfileState extends State<Profile> {
+class _ProfilePageState extends State<ProfilePage>
+    with SingleTickerProviderStateMixin {
   late Map<String, dynamic> userData = {};
+  late TabController _tabController;
+  bool _showTitle = true;
+  late List<Map<String, dynamic>> userPosts = []; 
 
   @override
   void initState() {
     super.initState();
     fetchData();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   Future<void> fetchData() async {
-    final response = await http.get(Uri.parse(ApiRoutes.getUserById(widget.user_Id)));
-    if (response.statusCode == 200) {
-      setState(() {
-        userData = json.decode(response.body);
-      });
-    } else {
-      throw Exception('Failed to load user data');
+    try {
+      final response =
+          await http.get(Uri.parse(ApiRoutes.getUserById(widget.user_Id)));
+      if (response.statusCode == 200) {
+        setState(() {
+          userData = json.decode(response.body);
+          print('fetching user data');
+        });
+      } else {
+        throw Exception('Failed to load user data');
+      }
+
+      final postResponse = await http.get(
+          Uri.parse(ApiRoutes.getPostsByCreatorId(widget.user_Id)));
+      if (postResponse.statusCode == 200) {
+        setState(() {
+          userPosts = json.decode(postResponse.body).cast<Map<String, dynamic>>();
+          print('fetching user posts');
+        });
+      } else {
+        throw Exception('Failed to load user posts');
+      }
+    } catch (error) {
+      print(error.toString());
+      // Handle error, show error message, retry logic, etc.
     }
   }
 
@@ -45,122 +75,173 @@ class _ProfileState extends State<Profile> {
     }
   }
 
+  String _formatDate(String? date) {
+    if (date != null && date.isNotEmpty) {
+      final parsedDate = DateTime.parse(date);
+      return DateFormat.MMMd().add_y().format(parsedDate);
+    }
+    return ''; // Return an empty string if date is null or empty
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: userData.isEmpty
           ? Center(child: CircularProgressIndicator())
           : CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            backgroundColor: Colors.transparent,
-            expandedHeight: 280,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Colors.deepPurple, Colors.black],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                  ),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 80, left: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          CircleAvatar(
-                            radius: 50,
-                            backgroundImage: _getImageProvider(userData['profilePic']),
-                            backgroundColor: Colors.black,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 10, top: 10),
-                            child: SizedBox(
-                              height: 35,
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  // Add functionality for the button
-                                },
-                                child: Text('Edit Profile'),
-                              ),
+              slivers: <Widget>[
+                SliverAppBar(
+                  pinned: true,
+                  backgroundColor: Colors.deepPurple,
+                  expandedHeight: 280,
+                  flexibleSpace: LayoutBuilder(
+                    builder: (context, constraints) {
+                      _showTitle = constraints.biggest.height <= 100;
+                      return FlexibleSpaceBar(
+                        title: _showTitle
+                            ? Text(
+                                userData['userName'],
+                                style: TextStyle(fontSize: 20),
+                              )
+                            : null,
+                        background: Container(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [Colors.deepPurple, Colors.black],
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
                             ),
                           ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20, top: 10),
-                      child: Text(
-                        userData['userName'],
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 80, left: 10),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    CircleAvatar(
+                                      radius: 50,
+                                      backgroundImage: _getImageProvider(
+                                          userData['profilePic']),
+                                      backgroundColor: Colors.black,
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          left: 10, top: 10),
+                                      child: SizedBox(
+                                        height: 35,
+                                        child: ElevatedButton(
+                                          onPressed: () {
+                                            // Add functionality for the button
+                                          },
+                                          child: Text('Edit Profile'),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 20, top: 10),
+                                child: Text(
+                                  userData['userName'],
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 20),
+                                child: Text(
+                                  '${userData['karma']} karma • ${_formatDate(userData['created_at'])}',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.grey[400],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
+                      );
+                    },
+                  ),
+                  actions: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const Search()),
+                        );
+                      },
+                      icon: const Icon(Icons.search),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: Text(
-                        '${userData['karma']} karma • ${userData['created_at']}',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.grey[400],
-                        ),
+                    IconButton(
+                      onPressed: () {
+                        Share.share('https://www.reddit.com/user/${widget.user_Id}/');
+                        print("it is pressed");
+                      },
+                      icon: Transform.scale(
+                        scale: 0.55,
+                        child: Image.asset('assets/Icons/share.png'),
                       ),
                     ),
                   ],
                 ),
-              ),
-            ),
-            actions: [
-              IconButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Search()),
-                  );
-                },
-                icon: const Icon(Icons.search),
-              ),
-              IconButton(
-                onPressed: () {
-                  Share.share('https://www.reddit.com/user/${widget.user_Id}/');
-                  print("it is pressed");
-                },
-                icon: Transform.scale(
-                  scale: 0.55,
-                  child: Image.asset('assets/Icons/share.png'),
+                SliverToBoxAdapter(
+                  child: TabBar(
+                    controller: _tabController,
+                    tabs: [
+                      Tab(text: 'Posts'),
+                      Tab(text: 'Comments'),
+                      Tab(text: 'About'),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ],
-        currentIndex: 0,
-        selectedItemColor: Colors.amber[800],
-        onTap: (_) {},
-      ),
+                SliverFillRemaining(
+                  child: TabBarView(
+                    controller: _tabController,
+                    children: [
+                      _buildPostsContainer(),
+                      Container(),
+                      Container(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+  );
+}
 
+  Widget _buildPostsContainer() {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: userPosts.length,
+      itemBuilder: (context, index) {
+        final post = userPosts[index];
+        return ListTile(
+          title: Text(
+            post['redditName'] ?? '',           
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text(
+            post['title'] ?? '',
+            style: TextStyle(fontSize: 16),
+          ),
+          trailing: post['picture'] != null
+              ? Image.network(
+                  post['picture'],
+                  width: 100,
+                  height: 250,
+                  fit: BoxFit.cover,
+                )
+              : null,
+        );
+      },
     );
   }
 }
--- CROSS/lib/routes/Mock_routes.dart
+++ CROSS/lib/routes/Mock_routes.dart
@@ -4,16 +4,13 @@
 
 class ApiRoutes {
   static const String baseUrl = 'https://json-server-k6zb.onrender.com';
-  // static const String baseUrl = 'http://localhost:3000';
   static const String getPosts = '$baseUrl/getPosts';
   static const String hotPosts = '$baseUrl/hotPosts';
   static const String topPosts = '$baseUrl/topPosts';
   static const String bestPosts = '$baseUrl/bestPosts';
-  // static const String Posts = '$baseUrl/posts';
-
   static const String login = '$baseUrl/user';
   static String getUserById(int id) => '$baseUrl/user/$id';
-  static const String getPopular =
-      '$baseUrl/posts?_sort=votes,commentsNo&_order=desc,desc';
+  static String getPostsByCreatorId(int id) => '$baseUrl/posts/$id';
+  static const String getPopular = '$baseUrl/posts?_sort=votes,commentsNo&_order=desc,desc';
   static const String getRecentSearch = '$baseUrl/recentlySearched';
 }
