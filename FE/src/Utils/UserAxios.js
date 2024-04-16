30ea6a40fd21d1dab9f4c4e6cda596d331ec46de
Mahmoud Khaled <78917638+mahmoudzkhaledd@users.noreply.github.com>
2024-04-16 22:46:59 +0200

    fix user axios

--- FE/src/Utils/UserAxios.js
+++ FE/src/Utils/UserAxios.js
@@ -2,10 +2,17 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const userAxios = axios.create({
-    baseURL: "http://localhost:3002/",
+    baseURL: "https://c1ba-156-196-38-182.ngrok-free.app/api/",
+    withCredentials: true,
     
 })
+userAxios.interceptors.request.use(request => {
 
+    request.headers.set('token', `Bearer ${localStorage.getItem('token')}`);
+    return request;
+}, error => {
+    return Promise.reject(error);
+});
 
 userAxios.interceptors.response.use(response => {
     if (response.data.token != null) {
@@ -15,20 +22,20 @@ userAxios.interceptors.response.use(response => {
         toast(response.data.msg);
     }
     return response;
-}, 
-error => {
-    if (error.response == null) {
-        toast.error("Please check your internet connection and try again.");
-    } else if (error.response != null && error.response.data.msg != null) {
-        toast.error(error.response.data.msg);
-        error.message = null;
-    }
-    else if (error.response != null && error.response.data.errors != null && error.response.data.errors.length != 0) {
-        toast.error(error.response.data.errors[0].msg);
-        error.message = null;
-    }
-    return Promise.reject(error);
-});
+},
+    error => {
+        if (error.response == null) {
+            toast.error("Please check your internet connection and try again.");
+        } else if (error.response != null && error.response.data.msg != null) {
+            toast.error(error.response.data.msg);
+            error.message = null;
+        }
+        else if (error.response != null && error.response.data.errors != null && error.response.data.errors.length != 0) {
+            toast.error(error.response.data.errors[0].msg);
+            error.message = null;
+        }
+        return Promise.reject(error);
+    });
 
 
 export { userAxios };
