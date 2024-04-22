5627c7e76d7df4e26e20d5b4ae140068663d3814
karimmagdy <karimmagdy2871@gmail.com>
2024-04-23 01:44:33 +0200

    create community last update isa

--- Backend/src/routes/community.routes.ts
+++ Backend/src/routes/community.routes.ts
@@ -4,12 +4,14 @@ import {
   getCommunityOfUserAsModeratorHandler,
   createSubredditHandler,
 } from '../controller/community.controller';
+import validateResource from '../middleware/validateResource';
+import { createCommunitySchema } from '../schema/community.schema';
 
 const router = express.Router();
 
 router.get('/subreddits/mine/member', getCommunityOfUserAsMemeberHandler);
 router.get('/subreddits/mine/moderator', getCommunityOfUserAsModeratorHandler);
 
-router.post('/create_subreddit', createSubredditHandler);
+router.post('/create_subreddit', validateResource(createCommunitySchema), createSubredditHandler);
 
 export default router;
