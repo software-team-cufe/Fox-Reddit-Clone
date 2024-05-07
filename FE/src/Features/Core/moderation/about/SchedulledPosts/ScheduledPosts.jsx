b91ab821eb803f42298a642c928b5b13b7e21e70
Omar Adel ElBehairy <omaradelelbehairy@gmail.com>
2024-05-08 00:17:49 +0300

    scheduled posts modified

--- FE/src/Features/Core/moderation/about/SchedulledPosts/ScheduledPosts.jsx
+++ FE/src/Features/Core/moderation/about/SchedulledPosts/ScheduledPosts.jsx
@@ -1,10 +1,17 @@
+import { useParams } from "react-router-dom"
+import React from 'react'
+
 export default function ScheduledPosts() {
     
+const {community} = useParams();
+
     return (
-        <div className="grid grid-cols-2">
-            <div className="border-solid-2">hi</div>
-            <div className="border-solid-2">hi</div>
+        <div className="flex w-full mt-16">
+            <div className="border border-solid-2 grow flex-grow-2 w-2/3">
+                {community}
+            </div>
+            <div className="border border-solid-2 flex-grow-1 w-1/3"><div className="border border-bottom-2">RECURRING POSTS</div>no recurring posts</div>
         </div>
-    )
+    );
 
 }
