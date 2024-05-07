9c5154d3bbad531e9f5c0ab229814625f162fc82
Hossam Yasser <hossamyasseramer@gmail.com>
2024-05-08 02:27:27 +0300

    small change

--- CROSS/lib/features/auth/screens/signup_screen.dart
+++ CROSS/lib/features/auth/screens/signup_screen.dart
@@ -202,9 +202,7 @@ class _SignupScreenState extends State<SignupScreen> {
                     errorMessage = message;
                   });
                   if (errorMessage == null) {
-                    Future.delayed(const Duration(seconds: 5), () {
-                      Get.to(() => const LoginScreen());
-                    });
+                    Get.to(() => const LoginScreen());
                   }
                 },
               ),
