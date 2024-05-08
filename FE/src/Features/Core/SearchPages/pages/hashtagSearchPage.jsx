4feeaa50e59692d96bf150ea0fd23f174d332101
AnAs101AlAa <anas.ibrahim03@eng-st.cu.edu.eg>
2024-05-08 21:38:23 +0300

    fully donme hashtage now

--- FE/src/Features/Core/SearchPages/pages/hashtagSearchPage.jsx
+++ FE/src/Features/Core/SearchPages/pages/hashtagSearchPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-
+import {Link} from "react-router-dom";
 
 export default function HashtagSearchPage() {
   return (
-    <div className="w-full mx-atuo">
+    <div className="w-full mx-atuo flex flex-col gap-3">
         <img src={'/comingSoon.png'} className="w-32 mt-32 h-32 mx-auto" alt="coming soon" />
         <div className="text-center text-lg mx-auto font-semibold text-gray-500 mt-4">coming soon to fox...</div>
+        <div className="text-center text-lg mx-auto font-semibold text-gray-500 ">maybe go back and discover more of this beautiful NOT site</div>
+        <Link to="/" className="bg-orange-500 w-fit mx-auto hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-full mt-3">back to home</Link>
     </div>
   );
 }
