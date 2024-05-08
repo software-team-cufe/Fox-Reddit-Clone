4efc34f952865a8113d5edec10fa0ab0cf1a4422
AnAs101AlAa <anas.ibrahim03@eng-st.cu.edu.eg>
2024-05-08 21:34:00 +0300

    hashtag ui done

---
+++ FE/src/Features/Core/SearchPages/pages/hashtagSearchPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function HashtagSearchPage() {
+  return (
+    <div className="w-full mx-atuo">
+        <img src={'/comingSoon.png'} className="w-32 mt-32 h-32 mx-auto" alt="coming soon" />
+        <div className="text-center text-lg mx-auto font-semibold text-gray-500 mt-4">coming soon to fox...</div>
+    </div>
+  );
+}
