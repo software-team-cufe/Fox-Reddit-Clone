3cfe6c8522dfa7a7cacae18a2f9a61fbf3f95625
nouraldeen1 <nour.hassan1321@gmail.com>
2024-09-01 00:03:35 +0300

    final

--- FE/src/Utils/UserAxios.js
+++ FE/src/Utils/UserAxios.js
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 import { extractAxiosError } from "./Utils";
 
 const userAxios = axios.create({
-    baseURL: "https://app.fox-nour/",
+    baseURL: "https://app.fox-nour.com/",
     withCredentials: true,
 
 })
