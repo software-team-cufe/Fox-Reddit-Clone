bbfb2f7c8361a1fd3ac74b49250c9a24bd0c0338
nouraldeen1 <nour.hassan1321@gmail.com>
2024-08-31 23:13:36 +0300

    again

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:3001', 'http://devopsagmdmnfront.southafricanorth.cloudapp.azure.com'],
+    origin: ['http://localhost:3001', 'http://devopsagmdmnfront.southafricanorth.cloudapp.azure.com','https://app.fox-nour.com'],
     credentials: true,
   })
 );
