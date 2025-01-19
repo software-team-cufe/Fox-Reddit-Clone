686a2807e9d1106d29b3d2c8188356db93b1e9be
nouraldeen1 <nour.hassan1321@gmail.com>
2025-01-19 20:45:51 +0000

    front end done

---
+++ FE/docker-clean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Remove stopped containers
+docker container prune -f
+
+# Remove unused images
+docker image prune -a -f
+
+# Remove unused volumes
+docker volume prune -f
+
+# Remove unused networks
+docker network prune -f
+
+echo "Docker cleanup complete!"
